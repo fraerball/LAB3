3b349544aa78041750a5683049e03d0e80248bc9
Nick <tehnicp108@gmail.com>
2025-12-23 01:36:32 +0300

    Удалил container.h и publication.h, поскольку добавил их в папку инклуд

--- container.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "publication.h"
-
-typedef struct container container_t;
-
-container_t* container_create(void);
-void container_destroy(container_t* container);
-int container_add(container_t* container, const publication_t* pub);
-int container_get_count(const container_t* container);
