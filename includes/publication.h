cc87b60765e4f53f8aa9cf1071bdd6eb580e7e65
Nick <tehnicp108@gmail.com>
2025-12-23 07:58:34 +0300

    Добавил объявление функции для выделения памяти на куче и копирование там строк,чтобы после выхода из функции строка не исчезла,а также поменял нейминги для большего удобства

--- includes/publication.h
+++ includes/publication.h
@@ -1,18 +1,19 @@
 #pragma once
 
 #include <stddef.h>
+#include <stdlib.h>
 
 typedef struct {
-    char* title;
+    char* title_publication;
     char* author_surname;
     char* author_initials;
-    char* journal_name;
+    char* title_journal;
     unsigned short publication_year;
     unsigned short journal_volume;
-    int is_rinz;
-    int page_count;
-    int citation_count;
+    unsigned char is_rinz;
+    unsigned short page_quantity;
+    unsigned short citation_quantity;
 } publication_t;
 
 int publication_copy(publication_t* dest, const publication_t* src);
-void publication_free(publication_t* pub);
+void publication_free(publication_t* publication);
