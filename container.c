145e0e30ffa5e9dea052c8b2141d13967fcd8956
Nick <tehnicp108@gmail.com>
2025-12-23 12:32:39 +0300

    Добавил разрушение контейнера и удаление элемента из стека

--- container.c
+++ container.c
@@ -5,22 +5,41 @@ typedef struct stack_node{
     struct stack_node* next;
 } stack_node_t;
 
-struct container {
+struct container{
     stack_node_t* top;
     size_t size;
 };
 
-struct container_iterator {
+struct container_iterator{
     const container_t* container;
     stack_node_t* current;
     size_t position;
 };
 
-container_t* container_init() {
+container_t* container_init(){
     container_t* container = malloc(sizeof(container_t));
     if (container == NULL) return NULL;
     
     container->top = NULL;
     container->size = 0;
     return container;
+}
+
+void container_destroy(container_t* container){
+    if (container == NULL) return NULL;
+    
+    container_clear(container);
+    free(container);
+}
+int container_pop(container_t* container){
+    if (container == NULL || container->top == NULL) return 0;
+    
+    stack_node_t* temp = container->top;
+    container->top = container->top->next;
+    
+    publication_free(&temp->data);
+    free(temp);
+    container->size--;
+    
+    return 1;
 }
