08419bc89313484b3b09a700d55961dde3a59e4c
Nick <tehnicp108@gmail.com>
2025-12-23 03:01:37 +0300

    добавил структуры для контейнера и итератора

--- container.c
+++ container.c
@@ -4,3 +4,14 @@ typedef struct stack_node{
     publication_t data;
     struct stack_node* next;
 } stack_node_t;
+
+struct container {
+    stack_node_t* top;
+    size_t size;
+};
+
+struct container_iterator {
+    const container_t* container;
+    stack_node_t* current;
+    size_t position;
+};
