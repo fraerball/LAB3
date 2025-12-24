3ff9cc0658992e30ec7589f43cdf6c6a30b53d49
Nick <tehnicp108@gmail.com>
2025-12-24 08:39:43 +0300

    добавил обработку элементов командной строки

--- main.c
+++ main.c
@@ -27,10 +27,86 @@ static comparator_t choose_comparator(sort_type_t type, sort_view_t view){
 }
 
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argp[]){
 
-        SetConsoleOutputCP(65001);
-    SetConsoleCP(65001);
+        SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
     setlocale(LC_ALL, "ru_RU.UTF-8");
 
+        program_args_t args;
+    
+    if (!analysis_args(argc, argp, &args)) {
+        fprintf(stderr, "Ошибка разбора аргументов командной строки\n");
+        return 1;
+    }
+    
+    if (args.mode == MODE_NONE) {
+        return 0;
+    }
+    
+    container_t* publications = NULL;
+    int result = 0;
+    
+    switch (args.mode) {
+        case MODE_GENERATE:
+            if (!args.surnames_file) {
+                fprintf(stderr, "Ошибка: Для режима генерации должен быть указан файл с фамилиями\n");
+                fprintf(stderr, "Используйте --surnames=ФАЙЛ или -S ФАЙЛ\n");
+                result = 1;
+                break;
+            }
+            
+            publications = generate_random_publications(args.generate_quantity, args.surnames_file,args.words_file,args.initials_file);
+            
+            if (!publications) {
+                fprintf(stderr, "Ошибка: Не удалось сгенерировать публикации\n");
+                result = 1;
+                break;
+            }
+            
+            if (!write_publications_csv(publications, args.output_file)) {
+                fprintf(stderr, "Ошибка: Не удалось записать публикации\n");
+                result = 1;
+            }
+            break;
+            
+        case MODE_SORT:
+            publications = read_publications_csv(args.input_file);
+            if (!publications) {
+                fprintf(stderr, "Ошибка: Не удалось прочитать публикации\n");
+                result = 1;
+                break;
+            }
+            
+            sort(publications, choose_comparator(args.sort_type, args.sort_view));
+            
+            if (!write_publications_csv(publications, args.output_file)) {
+                fprintf(stderr, "Ошибка: Не удалось записать отсортированные публикации\n");
+                result = 1;
+            }
+            break;
+            
+        case MODE_PRINT:
+            publications = read_publications_csv(args.input_file);
+            if (!publications) {
+                fprintf(stderr, "Ошибка: Не удалось прочитать публикации\n");
+                result = 1;
+                break;
+            }
+            
+            if (!print_publications_table(publications, args.output_file)) {
+                fprintf(stderr, "Ошибка: Не удалось вывести таблицу публикаций\n");
+                result = 1;
+            }
+            break;
+            
+        default:
+            break;
+    }
+    
+    if (publications) {
+        container_destroy(publications);
+    }
+    
+    return result;
 }
