a1526e8bf76fc98e86a0d4ed6ff3841abc73f5c0
Nick <tehnicp108@gmail.com>
2025-12-24 07:12:05 +0300

    добавил сетлокаль для русского языка и статичную функцию для выбора компаратора, чтобы использовать её только в main.c

--- main.c
+++ main.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 #include <string.h>
+#include <locale.h>
+#include <windows.h>
+#include "container.h"
+#include "args.h"
+#include "sort.h"
 
-int main(int argc, char* argv[]) {
-    if (argc > 1) {
-        if (strcmp(argv[1], "--generate") == 0) {
-            printf("Генерация\n");
+static comparator_t choose_comparator(sort_type_t type, sort_view_t view){
+    if (view == SORT_YEAR){
+        if (type == SORT_ASC){
+            return cmp_year_asc;
+        } else{
+            return cmp_year_desc;
         }
-        else if (strcmp(argv[1], "--sort") == 0) {
-            printf("Сортировка\n");
-        }
-        else if (strcmp(argv[1], "--print") == 0) {
-            printf("Вывод таблицы\n");
-        }
-        else {
-            printf("Ошибка: %s\n", argv[1]);
+    } 
+    else if (view == SORT_CITATIONS){
+        if (type == SORT_ASC){
+            return cmp_cit_asc;
+        } else{
+            return cmp_cit_desc;
         }
     }
-    return 0;
+    else{
+        return cmp_year_asc;
+    }
+}
+
+
+int main(int argc, char* argv[]){
+
+        SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+
 }
