bb1281f53caa2597a8f87586b88d6b45a6f74166
Nick <tehnicp108@gmail.com>
2025-12-30 05:22:08 +0300

    убрал объявление ненужных функций

--- includes/io.h
+++ includes/io.h
@@ -8,5 +8,4 @@ int print_publications_table(const container_t* container, const char* filename)
 
 container_t* generate_random_publications(int quantity,  const char* surnames_file, const char* words_file, const char* initials_file);
 
-char** read_surnames_from_file(const char* filename, int* quantity);
-void free_surnames_array(char** surnames, int quantity);
+void free_txt();
