fc1e8642704eca8ea1b805d43b10f269bd9a7d79
Nick <tehnicp108@gmail.com>
2025-12-30 04:09:47 +0300

    написал более явные условия

--- publication.c
+++ publication.c
@@ -3,26 +3,27 @@
 #include <string.h>
 
 static char* copy_string(const char* str){
-    if (!str) return NULL;
+    if (str == NULL) return NULL;
+
     char* new_str = malloc(strlen(str) + 1);
-    if (new_str) {
+    if (new_str != NULL){
         strcpy(new_str, str);
     }
     return new_str;
 }
 
 int publication_copy(publication_t* dest, const publication_t* src){
-    if (!src || !dest) return 0;
+    if (src == NULL || dest == NULL) return 0;
     
     dest->title_publication = copy_string(src->title_publication);
-    dest->author_surname = copy_string(src->author_surname);
-    dest->author_initials = copy_string(src->author_initials);
-    dest->title_journal = copy_string(src->title_journal);
+    dest->author_surname   = copy_string(src->author_surname);
+    dest->author_initials  = copy_string(src->author_initials);
+    dest->title_journal    = copy_string(src->title_journal);
     
     dest->publication_year = src->publication_year;
-    dest->journal_volume = src->journal_volume;
-    dest->is_rinz = src->is_rinz;
-    dest->page_quantity = src->page_quantity;
+    dest->journal_volume   = src->journal_volume;
+    dest->is_rinz          = src->is_rinz;
+    dest->page_quantity    = src->page_quantity;
     dest->citation_quantity = src->citation_quantity;
     
     return 1;
@@ -38,22 +39,24 @@ void publication_free(publication_t* publication){
     free(publication->title_journal);
     
     publication->title_publication = NULL;
-    publication->author_surname = NULL;
-    publication->author_initials = NULL;
-    publication->title_journal = NULL;
+    publication->author_surname    = NULL;
+    publication->author_initials   = NULL;
+    publication->title_journal     = NULL;
 }
+
 int cmp_year_asc(const void* value1, const void* value2){
 
     const publication_t* publication_value1 = (const publication_t*)value1;
     const publication_t* publication_value2 = (const publication_t*)value2;
     
-        if (publication_value1->publication_year != publication_value2->publication_year){
-            return publication_value1->publication_year - publication_value2->publication_year;
-        }
+    if (publication_value1->publication_year != publication_value2->publication_year){
+        return publication_value1->publication_year - publication_value2->publication_year;
+    }
 
     return publication_value1->citation_quantity - publication_value2->citation_quantity;
 
 }
+
 int cmp_year_desc(const void* value1, const void* value2){
 
     return -cmp_year_asc(value1, value2);
@@ -61,6 +64,7 @@ int cmp_year_desc(const void* value1, const void* value2){
 }
 
 int cmp_cit_asc(const void* value1, const void* value2){
+
     const publication_t* publication_value1 = (const publication_t*)value1;
     const publication_t* publication_value2 = (const publication_t*)value2;
     
@@ -69,7 +73,7 @@ int cmp_cit_asc(const void* value1, const void* value2){
     }
 
     return publication_value1->publication_year - publication_value2->publication_year;
-    
+
 }
 
 int cmp_cit_desc(const void* value1, const void* value2){
